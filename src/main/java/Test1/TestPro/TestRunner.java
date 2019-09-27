629edd3985da3eb79f9fc8e72cde2e6e7578bff8
Pawan Yadav <pawan.yadav@infrapn.lan>
2019-09-27 15:21:34 +0530

    Adding junit workspace first time

---
+++ src/main/java/Test1/TestPro/TestRunner.java
@@ -0,0 +1,12 @@
+package Test1.TestPro;
+
+public class TestRunner 
+{	
+//    public static void main( String[] args )
+//    {        
+//    	System.out.println("Running tests-->");
+//        Result result = JUnitCore.runClasses(JUnitTests2.class);
+//        System.out.println(result.getFailureCount());
+        
+//    }
+}
---
+++ src/main/java/Test1/TestPro/TestString.java
@@ -0,0 +1,18 @@
+package Test1.TestPro;
+
+import com.google.common.hash.HashCode;
+
+public class TestString {
+	
+	public static void main(String[] args) {
+		testHashcode();
+
+	}
+
+	private static void testHashcode() {
+		String s = "infras";
+//		HashCode hCode = HashCode.fromString(s);
+		System.out.println(new TestString().hashCode());
+	}
+
+}
